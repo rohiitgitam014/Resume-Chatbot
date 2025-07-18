cac30df0d8a46375b706cafc5cc98fce7a895200
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2025-07-18 07:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 # ---- CONFIG ----
 st.set_page_config(page_title="Resume Chatbot", layout="wide")
-st.title("🤖Welcome To  Data Scientists Rohit kumar Chatbot ")
+st.title("🤖Welcome To  Data Scientist Rohit kumar Chatbot ")
 st.image("rohit.jpg",width= 300) 
 
 # ---- GEMINI API KEY INPUT ----
