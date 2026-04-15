f338a2202a4a719df0f446456332b3b80aa8054a
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2026-04-15 11:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ chunks = chunk_text(cleaned_resume)
 
 # ---- SESSION INIT ----
 if "chat" not in st.session_state:
-    model = genai.GenerativeModel("gemini-2.0-flash")  # stable model
+    model = genai.GenerativeModel("gemini-3-flash-preview")  # stable model
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
 
