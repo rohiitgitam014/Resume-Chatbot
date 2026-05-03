9c515ba1638e3b8130704c6be64a4cf4416de99a
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2026-05-03 15:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("🤖 Welcome To Data Scientist Rohit Kumar Chatbot")
 st.image("rohit.jpg", width=300)
 
 # ---- API KEY ----
-api_key = "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"
+api_key = "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"
 client = Groq(api_key=api_key)
 
 # ---- LOAD RESUME FROM LOCAL PDF ----
